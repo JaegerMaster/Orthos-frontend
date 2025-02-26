c04edc6005f8483bfe41e1fee9236dcc2ede5e12
JaegerMaster <sunny4sure+github@gmail.com>
2025-02-26 06:13:22 +0000

    Updated vite config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    host: '0.0.0.0',  // This makes the server listen on all network interfaces
     open: true
   },
   resolve: {
